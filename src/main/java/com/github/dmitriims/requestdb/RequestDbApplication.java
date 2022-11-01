722f852f68cd6940c79ac66f2411157c3cb80213
Dmitrii Soia <dmitrii.soia@protonmail.ch>
2022-11-01 16:27:53 +0300

    initial commit

---
+++ src/main/java/com/github/dmitriims/requestdb/RequestDbApplication.java
@@ -0,0 +1,13 @@
+package com.github.dmitriims.requestdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestDbApplication.class, args);
+    }
+
+}
